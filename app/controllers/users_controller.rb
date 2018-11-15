0892d14085f7f54898bcd39dbdf262dfe75941bd
cwvilain <cwvilain@gmail.com>
2018-11-15 15:32:19 -0500

    Add working sign-up form
    Add form with fields for name, email, password, password confirm. Use
    rails form tool to include csrf protection.  Add form styling.
    
    Update controller to include create function to add a new user to db
    if valid, and to re-load new page if invalid.  Add private user_params
    function to require user object and allow valid fields. Add success
    flash to render on-successful submit. Update routes to include create
    from post request at /signup.
    
    Add html & css to render error messages on new page if submission is
    invalid.  Add flash to application html to surface flash messages.
    
    Add integration test for all above.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,30 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
+    @user = User.new
   end
+
+  #create a new user in the database
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Thanks for signing up!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    #helper function for create to require a user object and permit only allowed
+    #fields
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/about', to: 'static_pages#about'
-  resources :users 
+  resources :users
   root 'static_pages#home'
 end
